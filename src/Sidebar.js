7635bf93c7d2ffb0540d9270aa5679f4d79dc2d7
Guneet Gulati <guneetgulati01@gmail.com>
2020-12-18 01:40:19 +0530

    side dash

---
+++ src/Sidebar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Sidebar.css";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <h3>Dashboard</h3>
+        <ExpandMoreIcon />
+      </div>
+
+      <div className="sidebar__channels">
+        
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
