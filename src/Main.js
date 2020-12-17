33edccc609b2bcad3bcd2bc24fe5554c4a612db1
Guneet Gulati <guneetgulati01@gmail.com>
2020-12-18 01:40:03 +0530

    main dash

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import "./Main.css";
+import Header from "./Header";
+
+
+function Chat() {
+
+  return (
+    <div className="chat">
+      <Header />
+      
+    </div>
+  );
+}
+
+export default Chat;
