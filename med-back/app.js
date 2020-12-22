f0288883a5b349dc5c22ba42a4a92adbcee61c46
Guneet Gulati <guneetgulati01@gmail.com>
2020-12-22 15:17:28 +0530

    srvr

---
+++ med-back/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors"
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const connection_url =
+"mongodb+srv://admin:CowhCQomQkjvQiB4@cluster0.prd2x.mongodb.net/medDbB?retryWrites=true&w=majority";
+
+
+mongoose.connect(connection_url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  mongoose.connection.once("open", () => {
+    console.log("DB connected");
+  });
+
+  app.get("/", (req, res) => {
+    res.send("hello world");
+  });
+
+
+app.listen(8080, () => {
+  console.log("Server has started");
+});
