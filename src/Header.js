0ac2cae9e4354cd47bca120ede45c53645f33626
Guneet Gulati <guneetgulati01@gmail.com>
2020-12-22 15:15:06 +0530

    header

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,29 @@
 import React from "react";
 import "./Header.css";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/user/userSlice";
+import { useHistory } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 
-const Header = ({ channelName }) => {
+const Header = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  const history = useHistory();
+
+  const logouut = () => {
+    dispatch(logout());
+
+    history.push("/");
+  };
+
   return (
     <div className="header">
       <div className="header__left"></div>
 
-      <div className="header__right"></div>
+      <div className="header__right">
+        <p>Hi,{user?.displayName}</p>
+        <Button onClick={logouut}>Logout</Button>
+      </div>
     </div>
   );
 };
