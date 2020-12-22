a5e482f1aad537737eae4f78e31f8b4736519547
Guneet Gulati <guneetgulati01@gmail.com>
2020-12-22 15:15:24 +0530

    sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "./Sidebar.css";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import SidebarChannel from "./SidebarChannel";
+
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar__top">
         <h3>Dashboard</h3>
-        <ExpandMoreIcon />
       </div>
 
       <div className="sidebar__channels">
-        
+        <SidebarChannel channelName={"Profile"} e={"profile"}/>
+        <SidebarChannel channelName={"Doctors"} e={"doctors"}/>
+        <SidebarChannel channelName={"Prescriptions"} e={"prescriptions"} />
+        <SidebarChannel channelName={"Past Consultations"} e={"past consultations"} />
+        <SidebarChannel channelName={"Discussion"} e={"discussion"}  />
       </div>
     </div>
   );
