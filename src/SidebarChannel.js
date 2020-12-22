d0a07c1b31f0bc3d996403911f64e212e6c6a4b6
Guneet Gulati <guneetgulati01@gmail.com>
2020-12-22 15:15:40 +0530

    side op

--- src/SidebarChannel.js
+++ src/SidebarChannel.js
@@ -1,20 +1,20 @@
 import React from "react";
 import "./SidebarChannel.css";
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/user/userSlice";
 
-function SidebarChannel({ id, channelName }) {
-  
+function SidebarChannel({ id, channelName, e }) {
+  const history = useHistory();
+  const user = useSelector(selectUser);
+
   const cHandler = () => {
-    console.log(channelName);
+    history.push(`/patient/${user.uid}/${e}`);
   };
 
   return (
     <div onClick={cHandler} className="chan">
-      <h4>
-        <span className="chan__hash">
-          #
-        </span>
-        {channelName}
-      </h4>
+      <h4>{channelName}</h4>
     </div>
   );
 }
