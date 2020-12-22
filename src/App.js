3fe922b1155920e915463b157c8863c67fdacb20
Guneet Gulati <guneetgulati01@gmail.com>
2020-12-22 15:14:41 +0530

    app rt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,67 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
 import Main from "./Main";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/user/userSlice";
+import {auth} from "./firebase";
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        console.log(authUser);
+
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+        
+      } else {
+        dispatch(logout());
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [dispatch]);
+
+
   return (
-    <div className="app">
-      <Sidebar />
-      <Main/>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+
+          <Route path="/patient/:nme/dashboard">
+            <Sidebar />
+            <Main />
+          </Route>
+
+          <Route path="/" exact>
+            <h1>hello</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
-}
+  }
 
 export default App;
