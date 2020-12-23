29334e45285f7254933ebdaa9900bb5419725a19
Guneet Gulati <guneetgulati01@gmail.com>
2020-12-23 18:03:49 +0530

    slice update

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -4,20 +4,24 @@ export const userSlice = createSlice({
   name: "user",
   initialState: {
     user: null,
+    details: null,
   },
   reducers: {
     login: (state, action) => {
       state.user = action.payload;
     },
     logout: (state) => {
-      state.user = null ;
+      state.user = null;
+    },
+    setDetails: (state, action) => {
+      state.details = action.payload;
+      
     },
   },
 });
 
-export const { login, logout } = userSlice.actions;
+export const { login, logout, setDetails } = userSlice.actions;
 
 export const selectUser = (state) => state.user.user;
-
+export const selectDetails = (state) => state.user.details;
 export default userSlice.reducer;
-  
