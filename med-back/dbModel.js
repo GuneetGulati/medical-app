c57b6f8b3ff53d8ceead6596a54f19289b8f4ee5
Guneet Gulati <guneetgulati01@gmail.com>
2020-12-25 21:25:55 +0530

    model update fix

--- med-back/dbModel.js
+++ med-back/dbModel.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
 const patientSchema = mongoose.Schema({
-    id:String,
-    name:String,
-    lastName:String,
-    age:Number,
-    gender:String,
-    phone:Number,
-    address:String
+  id: String,
+  name: String,
+  lastName: String,
+  age: Number,
+  gender: String,
+  phone: Number,
+  address: String,
+  check: Number,
 });
 
-export default mongoose.model("patient",patientSchema)
+export default mongoose.model("patient", patientSchema);
